7ce117ae811ac218d591f887709c46ed4b6310d5
MohammadHomsee <Mohammadhomsee@gmail.com>
2024-06-08 15:18:17 +0400

    a

--- video_generator.py
+++ video_generator.py
@@ -1,17 +1,6 @@
 import torch
 from diffusers import StableVideoDiffusionPipeline
 from diffusers.utils import load_image, export_to_video
-import requests
-from PIL import Image
-from io import BytesIO
-
-def load_image_with_headers(url, headers):
-    response = requests.get(url, headers=headers)
-    response.raise_for_status()  # Raise an exception for HTTP errors
-
-    # Open the image using PIL
-    image = Image.open(BytesIO(response.content))
-    return image
 
 class VideoGenerator:
   def setup(self):
@@ -23,13 +12,9 @@ class VideoGenerator:
     )
     self.pipeline.enable_model_cpu_offload()
 
-    headers = {
-        "Cookie": "ai_dock_token=df1a86bcdc67c261f3ea4ebb1780ef5772e0aefabbb5a6796262e700880e84dc"
-    }
-    image = load_image_with_headers(image_input_url, headers)
-    print(type(image))
-    # image = image.resize((1024, 576))
+    image = load_image(image_input_url)
+    image = image.resize((1024, 576))
 
-    # generator = torch.manual_seed(42)
-    # frames = self.pipeline(image, decode_chunk_size=8, generator=generator).frames[0]
-    # export_to_video(frames, output_path, fps=21)
+    generator = torch.manual_seed(42)
+    frames = self.pipeline(image, decode_chunk_size=8, generator=generator).frames[0]
+    export_to_video(frames, output_path, fps=21)
