12c4fd254989c2fb68dff35ae4a14d683f614a3b
MohammadHomsee <Mohammadhomsee@gmail.com>
2024-06-08 12:09:39 +0400

    update

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-import base64
+from video_generator import VideoGenerator
 
 app = FastAPI()
 
+generator = VideoGenerator()
+
 class Input(BaseModel):
   image_url:str
-  
+
 @app.get("/data")
 async def root():
     return {"message": "Hello World"}
 
 @app.post("/generate")
 async def generate(input:Input):
-    print(input.image_url)
-  
+    generator.generate(input.image_url)
