3ea01c42b8ea4017a261298dbca5ec801f42127b
MohammadHomsee <Mohammadhomsee@gmail.com>
2024-06-08 13:22:18 +0400

    a

--- main.py
+++ main.py
@@ -18,5 +18,5 @@ async def root():
 async def generate(input:Input):
     generator.generate(input.image_url)
 
-path = "test3.png"
-generator.generate(path)
+# path = "test3.png"
+# generator.generate(path)
